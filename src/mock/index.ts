53816935d0810f207ce2c327b04ad904d2409442
Yauheny Vial <mr.jk1629003@gmail.com>
2023-02-04 20:47:51 +0300

    init: app

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import {LocalGithubUser} from "types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "Jkwal",
+    "avatar": "https://avatars.githubusercontent.com/u/64038815?v=4",
+    "name": "Yauheny Vial",
+    "company": null,
+    "blog": "",
+    "location": "Belarus; Minsk",
+    "bio": null,
+    "twitter": null,
+    "repos": 8,
+    "followers": 0,
+    "following": 0,
+    "created": "2020-04-20T19:08:20Z",
+}
